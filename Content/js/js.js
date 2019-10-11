57bf1a76f97cd88daf8c8564ae6956bd72c8f132
Daniel Jensen <45940133+Darkrenga@users.noreply.github.com>
2019-10-11 13:44:18 +0200

    Add files via upload

--- Content/js/js.js
+++ Content/js/js.js
@@ -1,1 +1,31 @@
 // hej hej hej
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 5000); // Change image every 2 seconds
+}
+
+var myIndex2 = 0;
+carousel2();
+
+function carousel2() {
+  var i;
+  var x = document.getElementsByClassName("slidertekstMindre");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex2++;
+  if (myIndex2 > x.length) {myIndex2 = 1}    
+  x[myIndex2-1].style.display = "block";  
+  setTimeout(carousel2, 5000); // Change image every 2 seconds
+}
